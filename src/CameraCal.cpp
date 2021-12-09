af2b42a3d9259e00dd8476f3fa3ac07e112dffd4
Jack Naylor <35677585+nackjaylor@users.noreply.github.com>
2021-12-09 14:42:05 +1100

    Shear params

--- src/CameraCal.cpp
+++ src/CameraCal.cpp
@@ -54,6 +54,25 @@ CameraCal::CameraCal(double fx, double fy, int img_w, int img_h, double ppx, dou
 
 }
 
+
+CameraCal::CameraCal(double fx, double fy, int img_w, int img_h, double ppx, double ppy, double k1, double k2, double k3, double p1, double p2, double b1, double b2)
+    :   m_fx(fx),
+        m_fy(fy),
+        m_img_w(img_w), 
+        m_img_h(img_h),
+        m_ppx(ppx),
+        m_ppy(ppy),
+        m_k1(k1),
+        m_k2(k2),
+        m_k3(k3),
+        m_p1(p1),
+        m_p2(p2),
+        m_b1(b1),
+        m_b2(b2),
+        m_camera_matrix(Eigen::Matrix3d::Identity()) {
+
+}
+
 double CameraCal::getPPx() {
     return m_ppx;
 }
@@ -72,7 +91,8 @@ void CameraCal::constructMatrix() {
 
 Eigen::Matrix3d CameraCal::new_matrix() {
     Eigen::Matrix3d cal_matrix = Eigen::Matrix3d::Identity();
-    cal_matrix(0,0) = m_fx/m_pix_size;
+    cal_matrix(0,0) = m_fx/m_pix_size+b1;
+    cal_matrix(0,1) = b2;
     cal_matrix(1,1) = m_fy/m_pix_size;
     cal_matrix(0,2) = m_ppx;
     cal_matrix(1,2) = m_ppy;
